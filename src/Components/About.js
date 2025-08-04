c31dfeb9e9da348c618e2e73765eb59b7e1772ab
Dhavaldave121002 <dhavaldave121002@gmail.com>
2025-08-04 11:28:07 +0530

    React

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,32 @@
+import { Component } from "react";
 import ProfileClass from "./ClassbasedComponentprofile";
 import FunctionalProfile from "./FunctionalComponentprofile";
 
-const About=()=>{
-  return(
-    <div>
-      {/* <h1>About</h1> */}
-      <FunctionalProfile name="Dhaval" age="20" address="Ahmedabad" gender="Male" email="d@gmail.com" mobile="1234567890"/>
-      <ProfileClass name="Devil" age="20" address="Ahmedabad" gender="Male" email="d@gmail.com" mobile="1234567890"/>
-    </div>
-  )
+// const About=()=>{
+//   return(
+//     <div>
+//       {/* <h1>About</h1> */}
+//       <FunctionalProfile name="Dhaval" age="20" address="Ahmedabad" gender="Male" email="d@gmail.com" mobile="1234567890"/>
+//       <ProfileClass name="Devil" age="20" address="Ahmedabad" gender="Male" email="d@gmail.com" mobile="1234567890"/>
+//     </div>
+//   )
+// }
+class About extends Component{
+  constructor(){
+    super();
+    console.log("constructor");
+  }
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+  render(){
+    console.log("render");
+    return(
+      <div>
+       <ProfileClass name="Devil" age="20" address="Ahmedabad" gender="Male" email="d@gmail.com" mobile="1234567890"/>
+       <ProfileClass name="Devil" age="20" address="Ahmedabad" gender="Male" email="d@gmail.com" mobile="1234567890"/>
+      </div>
+    )
+  }
 }
 export default About;
